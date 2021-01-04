5a9245804e28784d974e4853673ea9155c74df57
Hamza Nazir <hamza@trimulabs.com>
2021-01-04 11:44:39 +0500

    removed extra code and warnings

--- App/components/PodcastEpisode/index.js
+++ App/components/PodcastEpisode/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {View, Text, Image, FlatList, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
 import FontTelloIcon from '../../components/FontTelloIcon';
 import NotFound from '../NotFound';
 import style from './style';
@@ -11,30 +18,33 @@ const PodcastEpisode = ({podcastList, onPress}) => {
 
   return (
     <View style={style.parentStyle}>
-      <FlatList
-        data={podcastList}
-        keyExtractor={(pod) => pod.key}
-        renderItem={({item}) => {
-          return (
-            <TouchableOpacity onPress={() => onPress(item.key)}>
-              <View style={style.listBarStyle}>
-                <Image style={style.imageStyle} source={item.imageUrl} />
-                <View style={style.textWrap}>
-                  <Text numberOfLines={1} style={style.titleStyle}>
-                    {item.title}
-                  </Text>
-                  <Text numberOfLines={1} style={style.detailStyle}>
-                    {item.detail}
-                  </Text>
+      <SafeAreaView style={{flex: 1}}>
+        <FlatList
+          data={podcastList}
+          // Added toString to remove a warning
+          keyExtractor={(pod) => pod.key.toString()}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity onPress={() => onPress(item.key)}>
+                <View style={style.listBarStyle}>
+                  <Image style={style.imageStyle} source={item.imageUrl} />
+                  <View style={style.textWrap}>
+                    <Text numberOfLines={1} style={style.titleStyle}>
+                      {item.title}
+                    </Text>
+                    <Text numberOfLines={1} style={style.detailStyle}>
+                      {item.detail}
+                    </Text>
+                  </View>
+                  <View style={style.iconCameraStyle}>
+                    <FontTelloIcon name="right-dir" color={'#000'} size={25} />
+                  </View>
                 </View>
-                <View style={style.iconCameraStyle}>
-                  <FontTelloIcon name="right-dir" color={'#000'} size={25} />
-                </View>
-              </View>
-            </TouchableOpacity>
-          );
-        }}
-      />
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </SafeAreaView>
     </View>
   );
 };
--- App/components/index.js
+++ App/components/index.js
@@ -3,7 +3,6 @@ import FontTelloIcon from './FontTelloIcon';
 import Header from './Header';
 import LatestEpisode from './LatestEpisode';
 import LiveNow from './LiveNow';
-import BottomNavigationTab from './BottomNavigationTab';
 import Heading from './Heading';
 import PodcastEpisode from './PodcastEpisode';
 import NotFound from './NotFound';
@@ -16,5 +15,4 @@ export {
   Heading,
   PodcastEpisode,
   NotFound,
-  BottomNavigationTab,
 };
